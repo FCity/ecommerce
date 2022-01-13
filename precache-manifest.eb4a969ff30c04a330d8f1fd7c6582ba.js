f830e3e42a3bb34b31330286f17b5fbd6725bb75
Francisco Calderon <thefcalderon@gmail.com>
2022-01-12 22:49:47 -0800

    Updates

---
+++ precache-manifest.eb4a969ff30c04a330d8f1fd7c6582ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca1e491a7cad3a548680cc48359a118",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "07f91650e9ec4ee379b0",
+    "url": "/ecommerce/static/css/main.1f26e31f.chunk.css"
+  },
+  {
+    "revision": "36363495213e8188eff2",
+    "url": "/ecommerce/static/js/2.03878afe.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/ecommerce/static/js/2.03878afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f91650e9ec4ee379b0",
+    "url": "/ecommerce/static/js/main.6570e676.chunk.js"
+  },
+  {
+    "revision": "831a87ab3375c7c427c2",
+    "url": "/ecommerce/static/js/runtime-main.ee40b3dc.js"
+  }
+]);
